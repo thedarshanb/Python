0dd9cd37131f741cbf1578ab8e166d0662ad0221
Darshan <wolfmind59@gmail.com>
2025-08-20 20:18:53 +0530

    03

---
+++ 03.py
@@ -0,0 +1,20 @@
+#Input/Output Operation
+"""
+By using Input/output operation created Mini project finding age differnece
+"""
+
+x=input("Enter Boy name :")
+a=int(input("Enter Boy age :"))
+y=input("Enter Girl name :")
+b=int(input("Enter girl age :"))
+
+age_diff=abs(a-b) #uaing abs for absolute value
+
+print(f"{x} likes {y} their age difference is {age_diff}") # f-string 
+
+
+
+
+
+
+
