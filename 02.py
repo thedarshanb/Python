62b9890a5c54cdeea4d2ad2c21db010439aed411
Darshan <wolfmind59@gmail.com>
2025-08-20 20:17:52 +0530

    02

---
+++ 02.py
@@ -0,0 +1,51 @@
+#variable
+Name="Darshan"
+age=23
+student=True
+print(Name)
+print(age)
+print(student)
+
+#Data Types & Type Check
+x=10
+y="Jhon"
+z=True
+f=20.0
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(f))
+
+#Type Conversion
+x = "10"  
+y = int(x)  
+z = float(y) 
+print(z)  
+
+age=1
+s="99"
+print(int(s)+age)
+
+#Arthematic Operation
+a=10
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+
+#variable Reassignment
+x=5
+print(x)
+x=10
+print(x)
+
+
+
+
+
+
+
